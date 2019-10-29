f955a6593961b20e562be38096d542e98922e552
Ankit Pandoh <Pandoh.2007@gmail.com>
2019-10-29 15:44:52 +0000

    solution

---
+++ src/arrays/SmallestPositiveMissingNumber.java
@@ -0,0 +1,188 @@
+package arrays;
+
+import java.io.*;
+import java.util.InputMismatchException;
+
+public class SmallestPositiveMissingNumber {
+
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        OutputWriter out = new OutputWriter(System.out);
+        int t = in.readInt();
+        while (t-- > 0) {
+            int n = in.readInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = in.readInt();
+            }
+            // seggregate positive and negative numbers
+            int ans = 0;
+            int j = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] <= 0) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    j++;
+                }
+            }
+
+            // copy only the positive numbers
+            int current_size = n - j;
+            int[] current_arr = new int[current_size];
+
+            for (int i = j, k = 0; i < n; i++, k++) {
+                current_arr[k] = arr[i];
+
+            }
+
+            // make numbers negative as we find them only if they fall in range
+            for (int i = 0; i < current_arr.length; i++) {
+                int x = Math.abs(current_arr[i]);
+                if (x - 1 < current_arr.length && current_arr[x - 1] > 0) {
+                    current_arr[x - 1] = -current_arr[x - 1];
+                }
+            }
+
+            // return the index of first positive numbers which is the missing number
+            // else return current_arr.length + 1a
+            for (int i = 0; i < current_arr.length; i++) {
+                if (current_arr[i] > 0) {
+                    ans = i + 1;
+                    break;
+                }
+            }
+            ans = ans == 0 ? current_arr.length : ans;
+            out.printLine(ans);
+        }
+        out.close();
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public String readLine() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isEndOfLine(c));
+            return res.toString();
+        }
+
+        public String readString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public long readLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int readInt() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public boolean isEndOfLine(int c) {
+            return c == '\n' || c == '\r' || c == -1;
+        }
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    writer.print(' ');
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+    }
+}
